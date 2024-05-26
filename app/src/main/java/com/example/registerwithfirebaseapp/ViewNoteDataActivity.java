863cd2f6958b3a3e5ff1fa3de00ddafac0b32341
KosGeny <k_genne@mail.ru>
2024-05-27 01:18:28 +0300

    Notes with DB. Add and Update functions access

--- app/src/main/java/com/example/registerwithfirebaseapp/ViewNoteDataActivity.java
+++ app/src/main/java/com/example/registerwithfirebaseapp/ViewNoteDataActivity.java
@@ -2,8 +2,19 @@ package com.example.registerwithfirebaseapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.registerwithfirebaseapp.Notes.NoteModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ViewNoteDataActivity extends AppCompatActivity {
 
@@ -13,10 +24,37 @@ public class ViewNoteDataActivity extends AppCompatActivity {
         setContentView(R.layout.activity_view_note_data);
         String id = getIntent().getStringExtra("id");
 
-        TextView note_title = findViewById(R.id.note_title_view);
-        TextView note_content = findViewById(R.id.note_content_view);
+        TextView note_title = findViewById(R.id.note_title_view_edit);
+        TextView note_content = findViewById(R.id.note_content_view_edit);
+
+        ImageView save_note_btn = findViewById(R.id.save_note_btn);
+        ImageView back_note_btn = findViewById(R.id.arrow_back_btn);
+
+        save_note_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNotes(id, note_title.getText().toString(), note_content.getText().toString());
+            }
+        });
+
+        back_note_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewNoteDataActivity.this, NotesActivity.class));
+            }
+        });
 
         note_title.setText(getIntent().getStringExtra("note_title"));
         note_content.setText(getIntent().getStringExtra("note_content"));
+    }
+
+    private void saveNotes(String id, String note_title, String note_content){
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseUser firebaseUser = auth.getCurrentUser();
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Registered Users").child(firebaseUser.getUid()).child("yes").child("notes_list");
+        NoteModel noteModel = new NoteModel(id, note_title, note_content);
+        databaseReference.child(id).setValue(noteModel);
+
+        startActivity(new Intent(ViewNoteDataActivity.this, NotesActivity.class));
     }
 }
